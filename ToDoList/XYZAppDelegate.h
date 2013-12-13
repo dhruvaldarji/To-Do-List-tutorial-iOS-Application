e4dff8dd77fb75e34b068d39181b6e1d41b5bb4e
dhruvaldarji <dhruvaldarji@gmail.com>
2013-12-12 21:40:55 -0500

    Initial Commit

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Dhruval's MacBook on 12/12/13.
+//  Copyright (c) 2013 Dhruval Darji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
